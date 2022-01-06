6378188aa2a980e525538569f2dba1e2ab9cc2b6
Abdoul Kader <derokabore@gmail.com>
2022-01-06 22:35:20 +0100

    New Experiment with DT

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,7 +1,7 @@
 import pickle # Serialiser des objets (y comporis des modeles) joblib
 
 import pandas as pd
-from sklearn.svm import LinearSVC
+from sklearn.tree import DecisionTreeClassifier
 
 from config import Config
 
@@ -11,7 +11,7 @@ X_train = pd.read_csv(str(Config.FEATURES_PATH / "train_features.csv"))
 y_train = pd.read_csv(str(Config.FEATURES_PATH / "train_labels.csv"))
 
 # Entrainement du model
-model = LinearSVC()
+model = DecisionTreeClassifier()
 model.fit(X_train, y_train.to_numpy().ravel()) # e.g. array([[1], [0]]).ravel() = array([1, 0])
 
 # Enregisrement du model
